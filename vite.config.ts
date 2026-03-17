7c62ea8a6de15f936f7fec9fd5f9cc3b2ee6c8db
Brian Levi <bra.levi@gmail.com>
2026-03-17 15:32:04 +0100

    feat: introduced first portfolio blog

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         'blog-dca': resolve(__dirname, 'blog/what-is-dca.html'),
         'blog-inflation': resolve(__dirname, 'blog/beat-inflation.html'),
+        'blog-first-portfolio': resolve(__dirname, 'blog/first-portfolio.html'),
       },
     },
   },
